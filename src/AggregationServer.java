fe5042ec4e8d35965c738b9b4944c1b4714865ef
sam9525 <sam9985246327@gmail.com>
2024-09-17 16:11:29 +0930

    Create weather handler to handle GET and PUT

--- src/AggregationServer.java
+++ src/AggregationServer.java
@@ -1,3 +1,5 @@
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
 import com.sun.net.httpserver.HttpServer;
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -13,5 +15,26 @@ public class AggregationServer {
     server.setExecutor(Executors.newCachedThreadPool());
     server.start();
     System.out.println("Server is running on port: " + port);
+  }
+
+  static class WeatherHandler implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+      String method = exchange.getRequestMethod();
+
+      if (method.equalsIgnoreCase("GET")) {
+        handleGetRequest(exchange);
+      } else if (method.equalsIgnoreCase("PUT")) {
+        handlePutRequest(exchange);
+      } else {
+        exchange.sendResponseHeaders(400, 0);
+        exchange.close();
+      }
+    }
+
+    private void handleGetRequest(HttpExchange exchange) throws IOException {}
+
+    private void handlePutRequest(HttpExchange exchange) throws IOException {}
   }
 }
